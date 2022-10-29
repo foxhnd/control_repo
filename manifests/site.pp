d30fddb80d845aae1b75afbaff6eab00e3d446d2
foxhnd <psichokillah@gmail.com>
2022-10-29 10:23:40 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,16 @@
 node default {
      }
+
 node 'pup-learn' {
   include role::master_server
+}
+
+node /^web/ {
+     include role::app_server
+
+}
+
+node /^db/ {
+     include role::db_server
+
 }
